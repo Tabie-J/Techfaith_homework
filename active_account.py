ea15d9d5516d5da07898775299afe773a4cc8cd6
Tabie-J <151495354+Tabie-J@users.noreply.github.com>
2025-10-11 20:14:32 -0400

    Add files via upload

---
+++ active_account.py
@@ -0,0 +1,62 @@
+#Empty dictionary
+user_profile = {}
+
+#asking the user to enter their first name
+first_name = str(input("Enter your first name "))
+#put the first name in the empty dictionary
+user_profile["first name"] = first_name
+#asking the user to enter their last name
+last_name = str(input("Enter yout last name "))
+#put the user last name in the empty dictionary
+user_profile["last name"] = last_name
+#asking the user to put their username
+username = input("Enter your username ")
+#put the username in the empty dictionary
+user_profile["username"] = username
+#asking the user to enter their password
+password = input("What is your passsword?" )
+#put the password in the empty dictonary
+user_profile["password"] = password
+#asking the user to put their date of birth
+date_of_birth = input("Enter your date of birth (MM/DD/YYYY) ")
+#put the date of birth in the empty dictionary
+user_profile["date of birth"] = date_of_birth
+#asking the user to ad their phone number
+telephone = int(input("Enter your phone number "))
+#put the username in the empty dictionary
+user_profile["telephone"] = telephone
+
+#new key value
+user_profile["active_account"] = True
+
+#print the first name
+print("First name :",user_profile["first name"])
+#print the last name
+print("Last name: ", user_profile["last name"])
+#print the username
+print("username: ",username[username])
+#print password
+print("password: ",user_profile["password"])
+#print date of birth
+print("Date of Birth: ",user_profile["date of birth"])
+#print the phone number
+print("Phone number:",user_profile["telephone"])
+
+user_anwer = input("Do you want to attempt login")
+if user_anwer.lower() == "yes":
+    print("")
+#input()
+#max_attempts = 3
+for attempt in range (3):
+    username_input = input("Enter Username")
+    password_input = input("Enter password")
+    if (username_input == user_profile["username"] and password_input == user_profile["password"]):
+        if user_profile["active_account"]:
+            print("You're logged in",user_profile["first name"])
+            break
+        else:
+            print("Username or password failed")
+            if attempt:
+                print("You have locked your account. Closing script")
+            else:
+                print("Okay")
